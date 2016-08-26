8150f65aa431d19ba9b4c499efb1e8f8aebf99c5
TreasureSchool <lolzkiller1@gmail.com>
2016-08-26 09:39:39 +0200

    Exercise set 2 start.

---
+++ src/exercise/pkg2/Exercise2.java
@@ -0,0 +1,15 @@
+package exercise.pkg2;
+
+/**
+ * @author Joachim E. Christensen
+ */
+public class Exercise2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
